9f0f3695db521a0a3b06ca21f67f76e85f611867
Claudio Fontana <cfontana@suse.de>
2020-11-26 20:01:17 +0100

    accel: replace struct CpusAccel with AccelOpsClass
    centralize the registration of the cpus.c module
    accelerator operations in accel/accel-softmmu.c
    
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- accel/tcg/tcg-all.c
+++ accel/tcg/tcg-all.c
@@ -33,10 +33,6 @@
 #include "qemu/accel.h"
 #include "qapi/qapi-builtin-visit.h"
 
-#ifndef CONFIG_USER_ONLY
-#include "tcg-cpus.h"
-#endif /* CONFIG_USER_ONLY */
-
 struct TCGState {
     AccelState parent_obj;
 
@@ -112,16 +108,6 @@ static int tcg_init(MachineState *ms)
      * Initialize TCG regions
      */
     tcg_region_init();
-
-#ifndef CONFIG_USER_ONLY
-    if (mttcg_enabled) {
-        cpus_register_accel(&tcg_cpus_mttcg);
-    } else if (icount_enabled()) {
-        cpus_register_accel(&tcg_cpus_icount);
-    } else {
-        cpus_register_accel(&tcg_cpus_rr);
-    }
-#endif /* CONFIG_USER_ONLY */
     return 0;
 }
 
