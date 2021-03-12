67e8b0615fec0377007a850faef40fcd9bc25ae9
Claudio Fontana <cfontana@suse.de>
2021-03-12 14:49:59 +0100

    accel: add double dispatch mechanism for class initialization
    while on x86 all CPU classes can use the same set of TCGCPUOps,
    on ARM the right accel behavior depends on the type of the CPU.
    
    So we need a way to specialize the accel behavior according to
    the CPU. Therefore, add a second initialization, after the
    accel_cpu->cpu_class_init, that allows to do this.
    
    Signed-off-by: Claudio Fontana <cfontana@suse.de>
    Cc: Paolo Bonzini <pbonzini@redhat.com>

--- accel/accel-common.c
+++ accel/accel-common.c
@@ -54,9 +54,21 @@ static void accel_init_cpu_int_aux(ObjectClass *klass, void *opaque)
     CPUClass *cc = CPU_CLASS(klass);
     AccelCPUClass *accel_cpu = opaque;
 
+    /*
+     * double dispatch. The first callback allows the accel cpu
+     * to run initializations for the CPU,
+     * the second one allows the CPU to customize the accel cpu
+     * behavior according to the CPU.
+     *
+     * The second is currently only used by TCG, to specialize the
+     * TCGCPUOps depending on the CPU type.
+     */
     cc->accel_cpu = accel_cpu;
     if (accel_cpu->cpu_class_init) {
         accel_cpu->cpu_class_init(cc);
+    }
+    if (cc->init_accel_cpu) {
+        cc->init_accel_cpu(accel_cpu, cc);
     }
 }
 
